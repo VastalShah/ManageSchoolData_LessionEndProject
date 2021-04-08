4835da8fe44d47e056e63d110f49f055a8bf5525
Vatsal Shah <vashah@teksystems.com>
2021-04-08 12:50:05 +0530

    Added Base Controller

---
+++ Manage School Data/Controllers/BaseController.cs
@@ -0,0 +1,127 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manage_School_Data.Controllers
+{
+    public class BaseController<T> : Controller where T: BaseModel
+    {
+        private readonly IBaseRepository<T> _context;
+
+        public BaseController(IBaseRepository<T> context)
+        {
+            _context = context;
+        }
+
+        // GET: Classes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.GetAll());
+        }
+
+        // GET: Classes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var TModel = await _context.Get(id);
+            if (TModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(TModel);
+        }
+
+        // GET: Classes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Classes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(T TModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Insert(TModel);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(TModel);
+        }
+
+        // GET: Classes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var TModel = await _context.Get(id);
+            if (TModel == null)
+            {
+                return NotFound();
+            }
+            return View(TModel);
+        }
+
+        // POST: Classes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, T TModel)
+        {
+            if (id != TModel.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _context.Update(TModel);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(TModel);
+        }
+
+        // GET: Classes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var TModel = await _context.Get(id);
+            if (TModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(TModel);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _context.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Manage School Data/Controllers/ClassesController.cs
+++ Manage School Data/Controllers/ClassesController.cs
@@ -7,147 +7,19 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Manage_School_Data;
 using Manage_School_Data.Models;
+using DataLayer;
 
 namespace Manage_School_Data.Controllers
 {
-    public class ClassesController : Controller
+    public class ClassesController : BaseController<ClassesModel>
     {
-        private readonly MVC_Context _context;
-
-        public ClassesController(MVC_Context context)
+        public ClassesController(IBaseRepository<ClassesModel> repo) : base(repo)
         {
-            _context = context;
         }
 
-        // GET: Classes
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.ClassesModel.ToListAsync());
-        }
-
-        // GET: Classes/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var classesModel = await _context.ClassesModel
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (classesModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(classesModel);
-        }
-
-        // GET: Classes/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Classes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,Stream")] ClassesModel classesModel)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(classesModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(classesModel);
-        }
-
-        // GET: Classes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var classesModel = await _context.ClassesModel.FindAsync(id);
-            if (classesModel == null)
-            {
-                return NotFound();
-            }
-            return View(classesModel);
-        }
-
-        // POST: Classes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Stream")] ClassesModel classesModel)
-        {
-            if (id != classesModel.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(classesModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ClassesModelExists(classesModel.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(classesModel);
-        }
-
-        // GET: Classes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var classesModel = await _context.ClassesModel
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (classesModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(classesModel);
-        }
-
-        // POST: Classes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var classesModel = await _context.ClassesModel.FindAsync(id);
-            _context.ClassesModel.Remove(classesModel);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ClassesModelExists(int id)
+        /*//*private bool ClassesModelExists(int id)
         {
             return _context.ClassesModel.Any(e => e.ID == id);
-        }
+        }*/
     }
 }
